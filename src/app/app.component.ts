b1a5406799c216e4e42d80584489bf5024a25579
Christophe Faribault <chris@webisoft.com>
2024-03-04 09:45:35 -0500

    feature: Setup TypeORM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataSource } from 'typeorm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-typeorm';
+
+  constructor() {
+
+    const AppDataSource = new DataSource({
+      type: 'postgres',
+      host: 'host.docker.internal',
+      port: 15432,
+      username: 'postgres',
+      password: '<password>',
+      database: 'exmar',
+      synchronize: true,
+      logging: true,
+      entities: [],
+      subscribers: [],
+      migrations: [],
+    });
+    console.log('AppDataSource', AppDataSource);
+
+  }
 }
